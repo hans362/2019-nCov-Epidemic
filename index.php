50ebbfc0c114ab584abf81a9983ac2629265b98b
Hans362 <i@hans362.cn>
2020-01-30 08:13:30 +0000

    v1.3 - UI

--- index.php
+++ index.php
@@ -1,30 +1,149 @@
-<style type="text/css">
-table.gridtable {
-	font-family: verdana,arial,sans-serif;
-	font-size:18px;
-	color:#333333;
-	border-width: 1px;
-	border-color: #666666;
-	border-collapse: collapse;
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>2019-nCov 中国实时疫情信息聚合</title>
+
+<style>
+*{
+    box-sizing: border-box;
+    -webkit-box-sizing: border-box;
+    -moz-box-sizing: border-box;
 }
-table.gridtable th {
-	border-width: 1px;
-	padding: 8px;
-	border-style: solid;
-	border-color: #666666;
-	background-color: #dedede;
+body{
+    font-family: Helvetica;
+    -webkit-font-smoothing: antialiased;
+    background: rgba( 71, 147, 227, 1);
 }
-table.gridtable td {
-	border-width: 1px;
-	padding: 8px;
-	border-style: solid;
-	border-color: #666666;
-	background-color: #ffffff;
+h2{
+    text-align: center;
+    font-size: 24px;
+    letter-spacing: 1px;
+    color: white;
+    padding: 30px 0;
+}
+p{
+	text-align: center;
+	font-size: 14px;
+	color: white;
+}
+/* Table Styles */
+
+.table-wrapper{
+    margin: 20px 70px 70px;
+    box-shadow: 0px 35px 50px rgba( 0, 0, 0, 0.2 );
+}
+
+.fl-table {
+    border-radius: 5px;
+    font-size: 18px;
+    font-weight: normal;
+    border: none;
+    border-collapse: collapse;
+    width: 100%;
+    max-width: 100%;
+    white-space: nowrap;
+    background-color: white;
+}
+
+.fl-table td, .fl-table th {
+    text-align: center;
+    padding: 8px;
+}
+
+.fl-table td {
+    border-right: 1px solid #f8f8f8;
+    font-size: 16px;
+}
+
+.fl-table thead th {
+    color: #ffffff;
+    background: #4FC3A1;
+}
+
+
+.fl-table thead th:nth-child(odd) {
+    color: #ffffff;
+    background: #324960;
+}
+
+.fl-table tr:nth-child(even) {
+    background: #F8F8F8;
+}
+
+/* Responsive */
+
+@media (max-width: 767px) {
+    .fl-table {
+        display: block;
+        width: 100%;
+    }
+    .table-wrapper:before{
+        content: "Scroll horizontally >";
+        display: block;
+        text-align: right;
+        font-size: 11px;
+        color: white;
+        padding: 0 0 10px;
+    }
+    .fl-table thead, .fl-table tbody, .fl-table thead th {
+        display: block;
+    }
+    .fl-table thead th:last-child{
+        border-bottom: none;
+    }
+    .fl-table thead {
+        float: left;
+    }
+    .fl-table tbody {
+        width: auto;
+        position: relative;
+        overflow-x: auto;
+    }
+    .fl-table td, .fl-table th {
+        padding: 20px .625em .625em .625em;
+        height: 60px;
+        vertical-align: middle;
+        box-sizing: border-box;
+        overflow-x: hidden;
+        overflow-y: auto;
+        width: 120px;
+        font-size: 13px;
+        text-overflow: ellipsis;
+    }
+    .fl-table thead th {
+        text-align: left;
+        border-bottom: 1px solid #f7f7f9;
+    }
+    .fl-table tbody tr {
+        display: table-cell;
+    }
+    .fl-table tbody tr:nth-child(odd) {
+        background: none;
+    }
+    .fl-table tr:nth-child(even) {
+        background: transparent;
+    }
+    .fl-table tr td:nth-child(odd) {
+        background: #F8F8F8;
+        border-right: 1px solid #E6E4E4;
+    }
+    .fl-table tr td:nth-child(even) {
+        border-right: 1px solid #E6E4E4;
+    }
+    .fl-table tbody td {
+        display: block;
+        text-align: center;
+    }
 }
 </style>
-<table class="gridtable">
-<tr>
-	<th>数据来源</th><th>确诊</th><th>疑似</th><th>治愈</th><th>死亡</th>
+</head>
+<body>
+<h2>2019-nCov 中国实时疫情信息聚合</h2>
+<div class="table-wrapper">
+<table class="fl-table">
+	<thead><tr><th>数据来源</th><th>确诊</th><th>疑似</th><th>治愈</th><th>死亡</th></tr></thead>
+	<tbody>
     <?php
         include_once 'config.php';
         $result = mysqli_query($conn, "SELECT * from data");
@@ -38,5 +157,8 @@ table.gridtable td {
             echo "</tr>";
         }
     ?>
-</tr>
+    </tbody>
 </table>
+</div>
+<p>*表格中空白或显示 NULL 表示暂时无法获取到数据或数据来源不提供该项数据服务</p>
+</body>
