12a01b6efb213678b545df927aa6c1bc3707ba61
hans362 <i@hans362.cn>
2020-02-06 10:11:14 +0800

    v1.9 - Update medsci api

--- ms.php
+++ ms.php
@@ -1,29 +1,43 @@
 <?php
 $url = 'http://m.medsci.cn/wh.asp';
 $outPageTxt = file_get_contents($url);
+$outPageTxt = iconv("gb2312", "utf-8//IGNORE", $outPageTxt);
 $dom = new DOMDocument();
 @$dom->loadHTML($outPageTxt);
 $dom->normalize();
 $xpath = new DOMXPath($dom);
-$data = $xpath->query('/html/body/div[1]/p[2]/text()');
+$data = $xpath->query('/html/body/ul/li[1]/strong/text()');
 $res = '';
 for ($i = 0; $i < $data->length; $i++) {
     $items = $data->item($i);
     $text = $items->nodeValue;
-    $res = $res . $text . '<br/>';
+    $res = $res . $text;
 }
-$confirmed = '/(?<=\确诊 ).+?(?= 例)/';
-$suspected = '/(?<=\疑似  ).+?(?=  例)/';
-$cured = '/(?<=\治愈 ).+?(?= 例)/';
-$dead = '/(?<=\死亡 ).+?(?= 例)/';
-preg_match($confirmed, $res, $out);
-$confirmed = $out[0];
-preg_match($suspected, $res, $out);
-$suspected = $out[0];
-preg_match($dead, $res, $out);
-$dead = $out[0];
-preg_match($cured, $res, $out);
-$cured = $out[0];
+$confirmed = $res;
+$data = $xpath->query('/html/body/ul/li[2]/strong/text()');
+$res = '';
+for ($i = 0; $i < $data->length; $i++) {
+    $items = $data->item($i);
+    $text = $items->nodeValue;
+    $res = $res . $text;
+}
+$suspected = $res;
+$data = $xpath->query('/html/body/ul/li[4]/strong/text()');
+$res = '';
+for ($i = 0; $i < $data->length; $i++) {
+    $items = $data->item($i);
+    $text = $items->nodeValue;
+    $res = $res . $text;
+}
+$dead = $res;
+$data = $xpath->query('/html/body/ul/li[5]/strong/text()');
+$res = '';
+for ($i = 0; $i < $data->length; $i++) {
+    $items = $data->item($i);
+    $text = $items->nodeValue;
+    $res = $res . $text;
+}
+$cured = $res;
 include_once 'config.php';
 $sql = "UPDATE data SET confirmed=" . $confirmed . ", suspected=" . $suspected . ", cured=" . $cured . ", dead=" . $dead . " WHERE `source` = 'ms'";
 sqlMethod($sql);
